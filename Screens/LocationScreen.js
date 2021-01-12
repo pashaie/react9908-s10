1c64533448cf68e676e3288b82a8ff1cf84a23d8
pasha <pashaie@gmail.com>
2021-01-12 21:31:47 +0330

    location

--- Screens/LocationScreen.js
+++ Screens/LocationScreen.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {View, StyleSheet} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import {Button} from 'react-native-elements';
+import MapView, {Marker, Geojson} from 'react-native-maps';
+import Tehran from './tehran';
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 const LocationScreen = ({route, navigation}) => {
+  const map = useRef(null);
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocationGHALAT = () => {
+    // Geolocation.getCurrentPosition((info) => setLocation(info));
+    Geolocation.getCurrentPosition(setLocation);
+  };
+
   const getLocation = () => {
-    Geolocation.getCurrentPosition((info) => console.log(info));
+    Geolocation.getCurrentPosition((info) => {
+      setLocation({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+      console.log(info.coords);
+    });
   };
   return (
     <View>
-      <Text>LocationScreen</Text>
       <Button title="Get Location" type="outline" onPress={getLocation} />
+      {location && (
+        <MapView ref={map} style={styles.map} initialRegion={location}>
+          {/* <MapView.Circle center={location} radius={2000} /> */}
+          <Marker title="سلام" coordinate={location} draggable />
+          <Geojson
+            geojson={Tehran}
+            strokeColor="red"
+            fillColor="green"
+            strokeWidth={2}
+          />
+        </MapView>
+      )}
     </View>
   );
 };
