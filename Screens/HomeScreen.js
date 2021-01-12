a17b6086ff33f68ddedd0f74541c58af83f38a8a
pasha <pashaie@gmail.com>
2021-01-12 17:57:43 +0330

    icon

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import MenuScreen from './MenuScreen';
 import ProductScreen from './ProductScreen';
@@ -9,6 +10,11 @@ const Tab = createBottomTabNavigator();
 const HomeScreen = () => {
   return (
     <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          return <MaterialIcons name="360" />;
+        },
+      })}
       tabBarOptions={{
         activeTintColor: 'tomato',
         inactiveTintColor: 'gray',
