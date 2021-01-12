12a5d7cedcf2b0bfadb6d82defaa33be20e7a13f
pasha <pashaie@gmail.com>
2021-01-12 17:37:22 +0330

    stack navigator

---
+++ Screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const ProductScreen = () => {
+  return (
+    <View>
+      <Text>ProductScreen</Text>
+    </View>
+  );
+};
+
+export default ProductScreen;
