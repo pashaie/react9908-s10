2883f2321f9cea6c523235191ec5208696a5a62b
pasha <pashaie@gmail.com>
2021-01-12 17:46:14 +0330

    tab navigation

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import MenuScreen from './MenuScreen';
+import ProductScreen from './ProductScreen';
+
+const Tab = createBottomTabNavigator();
+
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+        activeBackgroundColor: 'purple',
+      }}>
+      <Tab.Screen name="Home" component={MenuScreen} />
+      <Tab.Screen name="Products" component={ProductScreen} />
+    </Tab.Navigator>
   );
 };
 
---
+++ Screens/MenuScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const MenuScreen = () => {
+  return (
+    <View>
+      <Text>MenuScreen</Text>
+    </View>
+  );
+};
+
+export default MenuScreen;
